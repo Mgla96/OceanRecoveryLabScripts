df8ecc6c6e276950eac44bc39e462ef8b791e396
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-02-07 14:10:10 -0800

    looping through all folders

--- alignanddelete.py
+++ alignanddelete.py
@@ -23,78 +23,72 @@ def main():
 	##parent = app.activeWindow()
 	
 	#prompting for path to photos
-	path_photos = PhotoScan.app.getExistingDirectory("Specify input photo folder:")
-	path_export = PhotoScan.app.getExistingDirectory("Specify EXPORT folder:")
-	doc.save(path_export+"\\"+"project.psx")
-	#processing parameters
-	accuracy = PhotoScan.Accuracy.HighAccuracy  #align photos accuracy
-	preselection = PhotoScan.Preselection.GenericPreselection
-	keypoints = 40000 #align photos key point limit
-	tiepoints = 10000 #align photos tie point limit
-	#source = PhotoScan.PointsSource.DensePoints #build mesh source
-	surface = PhotoScan.SurfaceType.Arbitrary #build mesh surface type
-	quality = PhotoScan.Quality.MediumQuality #build dense cloud quality
-	filtering = PhotoScan.FilterMode.AggressiveFiltering #depth filtering
-	interpolation = PhotoScan.Interpolation.EnabledInterpolation #build mesh interpolation
-	face_num = PhotoScan.FaceCount.HighFaceCount #build mesh polygon count
-	mapping = PhotoScan.MappingMode.GenericMapping #build texture mapping
-	atlas_size = 8192
-	blending = PhotoScan.BlendingMode.MosaicBlending #blending mode
-	color_corr = False
-	threshold=0.5
+	path_photos = PhotoScan.app.getExistingDirectory("Specify INPUT photo folder(containing all metashape files):")
 
-	print("Script started")
-	#creating new chunk
-	#doc.addChunk()
-	chunk = doc.chunks[-1]
-	chunk.label = "New Chunk"
+	path_export = PhotoScan.app.getExistingDirectory("Specify EXPORT folder:")	
 
-	#loading images
-	image_list = os.listdir(path_photos)
-	photo_list = list()
-	for photo in image_list:
-		if ("jpg" or "jpeg" or "JPG" or "JPEG") in photo.lower():
-			photo_list.append(path_photos + "\\" + photo)
-	chunk.addPhotos(photo_list)
-
-	#align photos
-	chunk.matchPhotos(accuracy = accuracy, preselection = preselection, filter_mask = False, keypoint_limit = keypoints, tiepoint_limit = tiepoints)
-	chunk.alignCameras()
-
-    #Removing points outside bounding box
-	for i in range(len(doc.chunks)):
-		chunk = doc.chunks[i]
-		R = chunk.region.rot		#Bounding box rotation matrix
-		C = chunk.region.center		#Bounding box center vertor
-		size = chunk.region.size
-		if not (chunk.point_cloud and chunk.enabled):
-			continue
-		elif not len(chunk.point_cloud.points):
-			continue
-		for point in chunk.point_cloud.points:
-			if point.valid:
-				v = point.coord
-				v.size = 3
-				v_c = v - C
-				v_r = R.t() * v_c	
-				if abs(v_r.x) > abs(size.x / 2.):
-					point.valid = False
-				elif abs(v_r.y) > abs(size.y / 2.):
-					point.valid = False
-				elif abs(v_r.z) > abs(size.z / 2.):
-					point.valid = False
-				else:
-					continue
-	print("Points outside the region were removed.")
-	
-	#Read reprojection error and delete any 0.5 or greater
-
-
-	#PhotoScan.app.addMenuItem("Custom menu/Filter point cloud by bounding box", main)	
-	#might be 2nd script below here after user defines points and set scale bar distance
-	doc.save()
-
+	fold_list = os.listdir(path_photos)
+	for folder in fold_list:
+		doc.save(path_export+"\\"+folder+".psx")
+		#processing parameters
+		accuracy = PhotoScan.Accuracy.HighAccuracy  #align photos accuracy
+		preselection = PhotoScan.Preselection.GenericPreselection
+		keypoints = 40000 #align photos key point limit
+		tiepoints = 10000 #align photos tie point limit
+		#source = PhotoScan.PointsSource.DensePoints #build mesh source
+		surface = PhotoScan.SurfaceType.Arbitrary #build mesh surface type
+		quality = PhotoScan.Quality.MediumQuality #build dense cloud quality
+		filtering = PhotoScan.FilterMode.AggressiveFiltering #depth filtering
+		interpolation = PhotoScan.Interpolation.EnabledInterpolation #build mesh interpolation
+		face_num = PhotoScan.FaceCount.HighFaceCount #build mesh polygon count
+		mapping = PhotoScan.MappingMode.GenericMapping #build texture mapping
+		atlas_size = 8192
+		blending = PhotoScan.BlendingMode.MosaicBlending #blending mode
+		color_corr = False
+		threshold=0.5
+		print("Script started")
+		#creating new chunk
+		#doc.addChunk()
+		chunk = doc.chunks[-1]
+		chunk.label = "New Chunk"
+		#loading images
+		image_list = os.listdir(path_photos)
+		photo_list = list()
+		for photo in image_list:
+			if ("jpg" or "jpeg" or "JPG" or "JPEG") in photo.lower():
+				photo_list.append(path_photos + "\\" + photo)
+		chunk.addPhotos(photo_list)
+		#align photos
+		chunk.matchPhotos(accuracy = accuracy, preselection = preselection, filter_mask = False, keypoint_limit = keypoints, tiepoint_limit = tiepoints)
+		chunk.alignCameras()
+    	#Removing points outside bounding box
+		for i in range(len(doc.chunks)):
+			chunk = doc.chunks[i]
+			R = chunk.region.rot		#Bounding box rotation matrix
+			C = chunk.region.center		#Bounding box center vertor
+			size = chunk.region.size
+			if not (chunk.point_cloud and chunk.enabled):
+				continue
+			elif not len(chunk.point_cloud.points):
+				continue
+			for point in chunk.point_cloud.points:
+				if point.valid:
+					v = point.coord
+					v.size = 3
+					v_c = v - C
+					v_r = R.t() * v_c	
+					if abs(v_r.x) > abs(size.x / 2.):
+						point.valid = False
+					elif abs(v_r.y) > abs(size.y / 2.):
+						point.valid = False
+					elif abs(v_r.z) > abs(size.z / 2.):
+						point.valid = False
+					else:
+						continue
+		print("Points outside the region were removed.")
+		#Read reprojection error and delete any 0.5 or greater
+		#PhotoScan.app.addMenuItem("Custom menu/Filter point cloud by bounding box", main)	
+		doc.save()
 PhotoScan.app.addMenuItem("Custom menu/Process 1", main)	
-
 main()
 print("Complete")
