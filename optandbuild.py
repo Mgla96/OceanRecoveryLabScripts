cc0035b7f2ccffd129f5dc1c181a88ef29b2bc83
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-02-08 22:52:18 -0800

    updates

--- optandbuild.py
+++ optandbuild.py
@@ -12,9 +12,11 @@ def main():
 
     global doc
 	doc = PhotoScan.app.document
+
 	#chunk = doc.chunk
 	path_photos = PhotoScan.app.getExistingDirectory("Specify INPUT photo folder(containing all alignanddelete metashape files):")
-	path_export = PhotoScan.app.getExistingDirectory("Specify EXPORT folder:")	
+	#don't need path export will put back in same place
+	#path_export = PhotoScan.app.getExistingDirectory("Specify EXPORT folder:")	
 	surface = PhotoScan.SurfaceType.Arbitrary #build mesh surface type
 	quality = PhotoScan.Quality.MediumQuality #build dense cloud quality
 	filtering = PhotoScan.FilterMode.AggressiveFiltering #depth filtering
@@ -26,34 +28,43 @@ def main():
 	color_corr = False
 	fold_list = os.listdir(path_photos)
 	for folder in fold_list:	
-		doc.save(path_export+"\\"+folder+".psx")
-		chunk.optimizeCameras()
-		#building dense cloud
-		PhotoScan.app.gpu_mask = 1  #GPU devices binary mask
-		PhotoScan.app.cpu_cores_inactive = 2  #CPU cores inactive
-		chunk.buildDenseCloud(quality = quality, filter = filtering)
-    	#Read reprojection Error and delete any 0.5 or greater
-		f = PhotoScan.PointCloud.Filter()
-		f.init(chunk, criterion=PhotoScan.PointCloud.Filter.ReprojectionError)
-		f.removePoints(0.5)
-			
-		#building mesh
-		chunk.buildModel(surface = surface, interpolation = interpolation, face_count = face_num)
-		#source = source,
-		
-		#build texture
-		chunk.buildUV(mapping = mapping, count = 1)
-		chunk.buildTexture(blending = blending , color_correction = color_corr, size = atlas_size)
-		PhotoScan.app.update()
-		#export
-		chunk.exportModel(path_export + "\\model.obj", format = "obj", texture_format='jpg')
-    	print("Script finished. Now saving.")
-    	try: 
-			doc.save()
-		except RuntimeError:
-			 PhotoScan.app.messageBox("Can't save project :()")
+		if ("psx" or "Psx") in folder.lower():
+			print(folder)
+			doc = PhotoScan.app.document
+			doc.open("folder")
+			chunk=doc.chunk
+			#optimize cameras
+			chunk.optimizeCameras()
 
+			#building dense cloud
+			chunk.buildDenseCloud()
+
+			#Read reprojection Error and delete any 0.5 or greater
+			f = PhotoScan.PointCloud.Filter()
+			f.init(chunk, criterion=PhotoScan.PointCloud.Filter.ReprojectionError)
+			f.removePoints(0.5)
+
+			#building dense cloud
+			chunk.buildDenseCloud(quality = quality, filter = filtering)
+    	
+			#building mesh
+			chunk.buildModel(surface = surface, interpolation = interpolation, face_count = face_num)
+		
+			#build texture
+			chunk.buildUV(mapping = mapping, count = 1)
+			chunk.buildTexture(blending = blending , color_correction = color_corr, size = atlas_size)
+			PhotoScan.app.update()
+			#export
+			chunk.exportModel(path_export + "\\model.obj", format = "obj", texture_format='jpg')
+    		print("Now saving.")
+			#saving docs
+			doc.save()
+		else:
+			continue
 PhotoScan.app.addMenuItem("Custom menu/Process 2", main)	
+t0 = time.time()
 main()
 t1 = time.time()
 PhotoScan.app.messageBox("Completed in "+ str(int(t1-t0))+"seconds.")
+
+			
