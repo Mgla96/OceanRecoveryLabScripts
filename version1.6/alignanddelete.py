7300e5c8af661017f923aaf85f67af343d573eed
Matthew Gottlieb <matthewdgottlieb@umail.ucsb.edu>
2020-03-16 18:36:38 -0700

    fixes for version 1.6

---
+++ version1.6/alignanddelete.py
@@ -0,0 +1,69 @@
+import os
+import PhotoScan
+import math, time
+'''
+alignanddelete will take a folder directory and loop through it's subfolders of separate coral treatments which consists of overlapping photos. 
+This script will create a new chunk add and align photos, create tie points, estimate camera locations and delete all tie points outside bounding box. 
+Then it will also delete pixels above a 0.5 reprojection error. Then this script will export these updated files to the user's designated location. 
+'''
+def main():
+    #prompting for path to photos
+    path_photos = PhotoScan.app.getExistingDirectory("Specify INPUT photo folder(containing all metashape files):")
+    path_export = PhotoScan.app.getExistingDirectory("Specify EXPORT folder:")
+    #processing parameters
+    downscale = 1 # Photo alignment accuracy - 1 is "high" not "highest"
+    generic_preselection = True  
+    keypoints = 40000 #align photos key point limit
+    tiepoints = 10000 #align photos tie point limit
+    threshold=0.5
+    fold_list = os.listdir(path_photos)
+    for folder in fold_list:
+        print("folder name is : "+folder)
+        #loading images
+        folderPath = path_photos + "\\" + folder
+        image_list = os.listdir(folderPath)
+        photo_list = list()
+        for photo in image_list:
+            if ("jpg" or "jpeg" or "JPG" or "JPEG") in photo.lower():
+                photo_list.append(os.path.join(folderPath,photo))
+        doc = PhotoScan.Document()
+        doc.save(path_export+"\\"+folder+".psx")
+        chunk=doc.addChunk() 
+        chunk.addPhotos(photo_list)
+        #align photos
+        chunk.matchPhotos(downscale = downscale, generic_preselection = generic_preselection, filter_mask = False, keypoint_limit = keypoints, tiepoint_limit = tiepoints)
+        chunk.alignCameras()
+        #Removing points outside bounding box
+        chunk = doc.chunks[-1]
+        R = chunk.region.rot #Bounding box rotation matrix
+        C = chunk.region.center #Bounding box center vertor
+        size = chunk.region.size
+        if not (chunk.point_cloud and chunk.enabled):
+            continue
+        elif not len(chunk.point_cloud.points):
+            continue
+        for point in chunk.point_cloud.points:
+            if point.valid:
+                v = point.coord
+                v.size = 3
+                v_c = v - C
+                v_r = R.t() * v_c
+                if abs(v_r.x) > abs(size.x / 2.):
+                    point.valid = False
+                elif abs(v_r.y) > abs(size.y / 2.):
+                    point.valid = False
+                elif abs(v_r.z) > abs(size.z / 2.):
+                    point.valid = False
+                else:
+                    continue
+        #Points outside the region were removed.
+        #Read reprojection error and delete any 0.5 or greater
+        f = PhotoScan.PointCloud.Filter()
+        f.init(chunk, criterion=PhotoScan.PointCloud.Filter.ReprojectionError)
+        f.removePoints(threshold)
+        doc.save()
+PhotoScan.app.addMenuItem("Custom menu/Process 1", main)
+t0 = time.time()
+main()
+t1 = time.time()
+PhotoScan.app.messageBox("Completed in "+str(int(t1-t0))+"seconds. Now define points & set scale bar distance before running optandbuild.py")
---
+++ version1.6/optandbuild.py
@@ -0,0 +1,71 @@
+import os
+import PhotoScan
+import math, time
+'''
+optandbuild will be called after the user defines points and set scale bar distance. 
+This script will optimize cameras, delete other points outside bounding box, build dense cloud, then delete more pixels above a 0.5 reprojection error. 
+Then it will repeat this process for all coral treatments.
+'''
+def main():
+	path_photos = PhotoScan.app.getExistingDirectory("Specify photo folder(containing all alignanddelete metashape files):")
+	surface = PhotoScan.SurfaceType.Arbitrary #build mesh surface type
+	downscale = 1 # Photo alignment accuracy - 1 is "high", 0 is "highest" replaces PhotoScan.Quality.HighQuality in version 1.5
+	filtering = PhotoScan.FilterMode.MildFiltering #depth filtering
+	interpolation = PhotoScan.Interpolation.EnabledInterpolation #build mesh interpolation
+	face_num = PhotoScan.FaceCount.HighFaceCount #build mesh polygon count
+	mapping = PhotoScan.MappingMode.GenericMapping #build texture mapping
+	atlas_size = 8192
+	blending = PhotoScan.BlendingMode.MosaicBlending #blending mode
+	fold_list = os.listdir(path_photos)
+	for folder in fold_list:	
+		if ("psx" or "Psx") in folder.lower():
+			print(folder)
+			doc = PhotoScan.app.document
+			doc.open(folder)
+			chunk=doc.chunk
+			#optimize cameras
+			chunk.optimizeCameras()
+			R = chunk.region.rot		#Bounding box rotation matrix
+			C = chunk.region.center		#Bounding box center vertor
+			size = chunk.region.size
+			if not (chunk.point_cloud and chunk.enabled):
+				continue
+			elif not len(chunk.point_cloud.points):
+				continue
+			for point in chunk.point_cloud.points:
+				if point.valid:
+					v = point.coord
+					v.size = 3
+					v_c = v - C
+					v_r = R.t() * v_c	
+					if abs(v_r.x) > abs(size.x / 2.):
+						point.valid = False
+					elif abs(v_r.y) > abs(size.y / 2.):
+						point.valid = False
+					elif abs(v_r.z) > abs(size.z / 2.):
+						point.valid = False
+					else:
+						continue
+			#Read reprojection Error and delete any 0.5 or greater
+			f = PhotoScan.PointCloud.Filter()
+			f.init(chunk, criterion=PhotoScan.PointCloud.Filter.ReprojectionError)
+			f.removePoints(0.5)
+			#building dense cloud
+			chunk.buildDepthMaps(quality = downscale, filter = filtering)
+			chunk.buildDenseCloud(point_colors = True)
+			#building mesh
+			chunk.buildModel(surface = surface, interpolation = interpolation, face_count = face_num)
+			#build texture
+			chunk.buildUV(mapping = mapping, count = 1)
+			chunk.buildTexture(blending = blending , size = atlas_size)
+			PhotoScan.app.update()
+			doc.save()
+		else:
+			continue
+PhotoScan.app.addMenuItem("Custom menu/Process 2", main)	
+t0 = time.time()
+main()
+t1 = time.time()	
+PhotoScan.app.messageBox("Completed in "+ str(int(t1-t0))+"seconds.")
+
+			
